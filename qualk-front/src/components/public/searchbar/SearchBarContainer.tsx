f6ad5c4ee95eaa0b7d6acec96c420134bd26139c
seobisback <66990091+sdf5771@users.noreply.github.com>
2023-03-02 22:45:11 +0900

    feat. 검색바 close 버튼 추가

--- qualk-front/src/components/public/searchbar/SearchBarContainer.tsx
+++ qualk-front/src/components/public/searchbar/SearchBarContainer.tsx
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import SearchBarPresenter from "./SearchBarPresenter";
 
 function SearchBarContainer(){
+    const [isActive, setIsActive] = useState(false);
+    const [isHover, setIsHover] = useState(false);
+    const [inputVal, setInputVal] = useState('');
+    const [visibleCloseBtn, setVisibleCloseBtn] = useState(false);
+    const inputOnClickHandler = (event: React.MouseEvent) => {
+        event.preventDefault()
+        setIsActive(true)
+    }
+
+    const visibleCloseHandler = (inputValue: string) => {
+        if(inputValue.length !== 0){
+            setVisibleCloseBtn(true);
+        } else {
+            setVisibleCloseBtn(false);
+        }
+    }
+
+    const inputOnChangeHandler = (event: React.FormEvent<HTMLInputElement>) => {
+        event.preventDefault();
+        const newValue = event.currentTarget.value;
+        setInputVal(newValue);
+        visibleCloseHandler(newValue);
+    }
+    const inputOnBlurHandler = (event: React.MouseEvent) => {
+        event.preventDefault()
+        setIsActive(false)
+    }
+    const mainContainerOnMouseOverHandler = (event: React.MouseEvent) => {
+        setIsHover(true)
+    }
+    const mainContainerOnMouseOutHandler = (event: React.MouseEvent) => {
+        setIsHover(false)
+    }
+
     return(
-        <SearchBarPresenter />
+        <SearchBarPresenter
+            inputOnClickHandler={inputOnClickHandler}
+            inputOnChangeHandler={inputOnChangeHandler}
+            inputOnBlurHandler={inputOnBlurHandler}
+            onMouseOverHandler={mainContainerOnMouseOverHandler}
+            onMouseOutHandler={mainContainerOnMouseOutHandler}
+            isHover={isHover}
+            isActive={isActive}
+            inputVal={inputVal}
+            isVisibleClose={visibleCloseBtn}
+        />
     );
 }
 
--- qualk-front/src/components/public/searchbar/SearchBarPresenter.tsx
+++ qualk-front/src/components/public/searchbar/SearchBarPresenter.tsx
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, {ReactEventHandler} from 'react';
 import styles from './SearchBarPresenter.module.css'
 import {ReactComponent as SearchIcon} from 'assets/images/workbook/searchbar/search_icon.svg';
+import {ReactComponent as SearchIconActive} from 'assets/images/workbook/searchbar/search_icon_active.svg';
+import {ReactComponent as CloseIcon} from 'assets/images/workbook/searchbar/close_icon.svg';
 
-function SearchBarPresenter(){
+type SearchBarPresenterPropsType = {
+    isActive: boolean,
+    isHover: boolean,
+    inputOnClickHandler: ReactEventHandler,
+    inputOnChangeHandler: ReactEventHandler,
+    inputOnBlurHandler: ReactEventHandler,
+    onMouseOverHandler: ReactEventHandler,
+    onMouseOutHandler: ReactEventHandler,
+    inputVal: string,
+    isVisibleClose: boolean,
+}
+
+function SearchBarPresenter({inputOnClickHandler, inputOnChangeHandler, inputOnBlurHandler, onMouseOverHandler, onMouseOutHandler, isActive, isHover, inputVal, isVisibleClose}:SearchBarPresenterPropsType){
+    let logo;
+
+    if(isActive || isHover){
+        logo = <SearchIconActive width="24px" height="24px"/>
+    } else {
+        logo = <SearchIcon width="24px" height="24px"/>
+    }
+
     return(
-        <div className={styles.search_bar_main}>
-            <SearchIcon width="24px" height="24px"/>
+        <div onMouseOver={onMouseOverHandler}
+             onMouseOut={onMouseOutHandler}
+             className={`${styles.search_bar_main} ${isActive ? styles.active : ''}`}
+        >
+            {logo}
             <div className={styles.input_container}>
-                <input className={styles.search_input} placeholder='오늘은 무엇을 찾아볼까요?' />
+                <input onClick={inputOnClickHandler}
+                       onChange={inputOnChangeHandler}
+                       onBlur={inputOnBlurHandler}
+                       className={styles.search_input}
+                       placeholder='오늘은 무엇을 찾아볼까요?'
+                       value={inputVal}
+                />
+                {isVisibleClose ? <CloseIcon /> : null}
             </div>
         </div>
     );
