4e0eb1894c9da02b045c8762a725a14330ff7d82
DESKTOP-L0CGF2I\kzsc5 <kzsc5464@naver.com>
2023-03-11 18:01:27 +0900

    no message

--- back/app/main.py
+++ back/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from route import question
 import pandas as pd
+import uvicorn
 
 app = FastAPI()
 app.include_router(question.router)
@@ -8,3 +9,7 @@ app.include_router(question.router)
 @app.get("/")
 async def root():
     return {"message": "Hello Bigger Applications!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
