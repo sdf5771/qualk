f92832477bef04397771c68cb10125c4b25c6d7f
DESKTOP-L0CGF2I\kzsc5 <kzsc5464@naver.com>
2023-03-21 00:57:33 +0900

    CORS

--- back/app/main.py
+++ back/app/main.py
@@ -1,10 +1,27 @@
 from fastapi import FastAPI
 from route import question
+from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 import uvicorn
 
 app = FastAPI()
 app.include_router(question.router)
+
+origins = [
+    "http://localhost",
+    "https://localhost",
+    "http://localhost",
+    "http://localhost:8000",
+    "https://qualk.co.kr"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
