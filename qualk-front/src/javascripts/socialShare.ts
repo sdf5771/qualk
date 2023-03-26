d17e060e017210fb98995634e425048b323cd66f
seobisback <66990091+sdf5771@users.noreply.github.com>
2023-03-26 18:51:28 +0900

    Update socialShare.ts

--- qualk-front/src/javascripts/socialShare.ts
+++ qualk-front/src/javascripts/socialShare.ts
@@ -27,10 +27,31 @@ export default function socialShare(social:string){
                     }
 
                     // @ts-ignore
-                    kakao.Share.sendScrap({
-                        templateId: 91754,
-                        requestUrl: baseUrl,
-                    });
+                    kakao.Share.sendDefault({
+                        objectType: 'feed',
+                        content: {
+                            title: 'Qualk',
+                            description: '여러분의 시험준비, 해결 해드릴게요!',
+                            imageUrl:
+                                'https://qualk.co.kr/logo512.png',
+                            link: {
+                                mobileWebUrl: baseUrl,
+                                webUrl: baseUrl,
+                            },
+                        },
+                    })
+                    // kakao.Share.sendScrap({
+                    //     templateId: 91754,
+                    //     requestUrl: baseUrl,
+                    // });
+                    //
+                    // kakao.Share.sendCustom({
+                    //     templateId: 91754,
+                    //     templateArgs: {
+                    //         title: '라이언이 즐겨먹던 바로 그 틴케이스 치즈볼',
+                    //         description: '바라만 봐도 즐거워지는 힐링 패키지에는 시크릿 스토리가 숨어있어요.',
+                    //     },
+                    // });
                 }
             }catch(e){
                 throw new Error("에러가 발생하였습니다.")
