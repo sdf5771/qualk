2b11237f5bf28d09c0c2b876011e9a2bb7e4fc72
seobisback <66990091+sdf5771@users.noreply.github.com>
2023-03-27 16:03:19 +0900

    Update NoContents.tsx
    upload svg component

--- qualk-front/src/components/public/no-contents/NoContents.tsx
+++ qualk-front/src/components/public/no-contents/NoContents.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from 'components/public/no-contents/NoContents.module.css';
+import {ReactComponent as NoContentsLogo} from 'assets/images/public/no_contents_logo.svg';
 
 function NoContents(){
     return(
         <div className={styles.no_contents_root}>
             <div className={styles.no_contents_logo_container}>
-                <div></div>
+                <NoContentsLogo />
             </div>
             <div className={styles.title}><span>아직 준비중 이에요...</span></div>
             <div className={styles.description}><span>아직 준비되지 않은 콘텐츠에요.. 현재 활성화된 콘텐츠를 방문해 볼까요?</span></div>
