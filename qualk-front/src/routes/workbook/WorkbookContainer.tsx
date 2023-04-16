8f0a0ff3e793f12f8df90bed8c4480397edaafd2
seobisback <66990091+sdf5771@users.noreply.github.com>
2023-04-16 20:58:31 +0900

    Update WorkbookContainer.tsx

--- qualk-front/src/routes/workbook/WorkbookContainer.tsx
+++ qualk-front/src/routes/workbook/WorkbookContainer.tsx
@@ -17,11 +17,11 @@ function WorkbookContainer(){
     // 잘못된 경로로 접속할 경우 임시 redirect
     useEffect(() => {
         if(location.pathname.split('/')[2] != 'gaiq'
-            || location.pathname.split('/')[2] != 'GAIQ'
-            || location.pathname.split('/')[2] != 'sqld'
-            || location.pathname.split('/')[2] != 'SQLD'
-            || location.pathname.split('/')[2] != 'sqid'
-            || location.pathname.split('/')[2] != 'SQID'){
+            && location.pathname.split('/')[2] != 'GAIQ'
+            && location.pathname.split('/')[2] != 'sqld'
+            && location.pathname.split('/')[2] != 'SQLD'
+            && location.pathname.split('/')[2] != 'sqid'
+            && location.pathname.split('/')[2] != 'SQID'){
             navigate('/quiz/gaiq')
         }
     }, [])
