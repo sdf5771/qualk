90f4297813e00a0afe3ce6140df844f92a59b88b
seobisback <66990091+sdf5771@users.noreply.github.com>
2023-04-16 20:00:00 +0900

    modify. http request uri

--- qualk-front/src/queries/workbook/listview/getQuestionFindOld.ts
+++ qualk-front/src/queries/workbook/listview/getQuestionFindOld.ts
@@ -1,8 +1,5 @@
-const LocalServer_URL = process.env.LocalServerURL;
-const RealServer_URL = process.env.RealServerURL;
-
 async function getQuestionFindOld(type: string, pageNumber:number){
-    const response = await fetch(`http://localhost:8000/api/v1/quiz/${type}/old/${pageNumber}`,{
+    const response = await fetch(`/api/v1/quiz/${type}/old/${pageNumber}`,{
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
