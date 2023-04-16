379f4bb91183ee30bf71948f31cd052c7b3726a4
seobisback <66990091+sdf5771@users.noreply.github.com>
2023-04-16 20:10:17 +0900

    update package.json and request uri

--- qualk-front/src/queries/workbook/listview/getQuestionFindNew.ts
+++ qualk-front/src/queries/workbook/listview/getQuestionFindNew.ts
@@ -1,5 +1,5 @@
 async function getQuestionFindNew(type: string, pageNumber:number){
-    const response = await fetch(`/api/v1/quiz/${type}/new/${pageNumber}`,{
+    const response = await fetch(`https://qualk.co.kr/api/v1/quiz/${type}/new/${pageNumber}`,{
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
--- qualk-front/src/queries/workbook/listview/getQuestionFindOld.ts
+++ qualk-front/src/queries/workbook/listview/getQuestionFindOld.ts
@@ -1,5 +1,5 @@
 async function getQuestionFindOld(type: string, pageNumber:number){
-    const response = await fetch(`/api/v1/quiz/${type}/old/${pageNumber}`,{
+    const response = await fetch(`https://qualk.co.kr/api/v1/quiz/${type}/old/${pageNumber}`,{
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
