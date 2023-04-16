62bc7ecf5774eb07f829871df8458f535c650152
seobisback <66990091+sdf5771@users.noreply.github.com>
2023-04-16 20:19:50 +0900

    modify. http request uri

--- qualk-front/src/queries/workbook/listview/getQuestionFindNew.ts
+++ qualk-front/src/queries/workbook/listview/getQuestionFindNew.ts
@@ -1,5 +1,5 @@
 async function getQuestionFindNew(type: string, pageNumber:number){
-    const response = await fetch(`http://localhost:8000/api/v1/quiz/${type}/new/${pageNumber}`,{
+    const response = await fetch(`/api/v1/quiz/${type}/new/${pageNumber}`,{
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
--- qualk-front/src/queries/workbook/listview/getQuestionFindView.ts
+++ qualk-front/src/queries/workbook/listview/getQuestionFindView.ts
@@ -1,5 +1,5 @@
 async function getQuestionFindView(type: string, pageNumber: number){
-    const response = await fetch(`http://localhost:8000/api/v1/quiz/${type}/view/${pageNumber}`,{
+    const response = await fetch(`/api/v1/quiz/${type}/view/${pageNumber}`,{
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
